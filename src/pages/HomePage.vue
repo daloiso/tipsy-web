c20fb4a55064f935a908c3b31b9cdb16cbb4048b
T. C <37833294+mcbaguetti@users.noreply.github.com>
2022-11-17 17:55:17 +0100

    first login concept without firebase

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="flex q-pa-md">
+    Welcome Home
+    <q-space />
+    <div>
+      <q-btn
+        class="flex flex-center q-px-lg q-py-sm q-mb-md"
+        size="md"
+        label="Logout"
+        @click="logout"
+        color="primary"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "HomePage",
+  data() {
+    return {};
+  },
+  methods: {
+    logout() {
+      console.log("logged out");
+    },
+  },
+};
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,21 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: "/", component: () => import("src/pages/AuthPage.vue") },
+      {
+        path: "/home",
+        component: () => import("src/pages/HomePage.vue"),
+        meta: { requiresAuth: true },
+      },
+    ],
   },
-
-  // Always leave this as last one,
+  // Always leave this as the last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
-
-export default routes
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/Error404.vue"),
+  },
+];
+export default routes;
