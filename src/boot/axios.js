b771d85ca31b0b0d214b1ebe742a4b58bff135e1
daloiso <pasquale.daloiso@gmail.com>
2022-12-10 19:12:28 +0100

    https fnzionante

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import axios from 'axios'
+import https from 'https'
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+})
+axios.defaults.httpsAgent = httpsAgent
 
 Vue.prototype.$axios = axios
+
 export const http = axios.create({
-  baseURL:'http://localhost:8080',
+  baseURL:'https://localhost:8443',
   })
 
 
